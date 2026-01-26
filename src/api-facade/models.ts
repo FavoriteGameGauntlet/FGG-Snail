3b08d352af621025d44f336c3c4bfe5b5c290802
Welnyr <thekotech@gmail.com>
2026-01-26 21:58:33 +0500

    chore: Update models

--- src/api-facade/models.ts
+++ src/api-facade/models.ts
@@ -83,42 +83,11 @@ export type EffectDto = {
 
 export type EffectsDto = EffectDto[]
 
-export type Effect = Omit<EffectDto, 'createDate' | 'rollDate'> & {
-	createDate: string
-	rollDate?: string
-}
+export type Effect = DtoStringToDate<EffectDto, 'createDate' | 'rollDate'>
 
 export type Effects = Effect[]
 
-export enum ExceptionCode {
-	UserNotFound = 'USER_NOT_FOUND',
-	GameNotFound = 'GAME_NOT_FOUND',
-	TimerNotFound = 'TIMER_NOT_FOUND',
-	NoAvailableRolls = 'NO_AVAILABLE_ROLLS',
-	UserAlreadyExists = 'USER_ALREADY_EXISTS',
-	TimerIncorrectState = 'TIMER_INCORRECT_STATE',
-	Unexpected = 'UNEXPECTED',
-	CheckUser = 'CHECK_USER',
-	CheckCurrentGame = 'CHECK_CURRENT_GAME',
-	CheckCurrentTimer = 'CHECK_CURRENT_TIMER',
-	GetUser = 'GET_USER',
-	GetUnplayedGames = 'GET_UNPLAYED_GAMES',
-	GetCurrentGame = 'GET_CURRENT_GAME',
-	GetCurrentTimer = 'GET_CURRENT_TIMER',
-	CreateUser = 'CREATE_USER',
-	AddUnplayedGames = 'ADD_UNPLAYED_GAMES',
-	StartCurrentTimer = 'START_CURRENT_TIMER',
-	PauseCurrentTimer = 'PAUSE_CURRENT_TIMER',
-	FinishCurrentGame = 'FINISH_CURRENT_GAME',
-	GetGameHistory = 'GET_GAME_HISTORY',
-	CurrentGameAlreadyExists = 'CURRENT_GAME_ALREADY_EXISTS',
-	MakeGameRoll = 'MAKE_GAME_ROLL',
-	CancelCurrentGame = 'CANCEL_CURRENT_GAME',
-	GetEffectHistory = 'GET_EFFECT_HISTORY',
-	CheckEffectRoll = 'CHECK_EFFECT_ROLL',
-}
-
-export type Exception = {
-	code: ExceptionCode
+export type ApiError = {
+	code: string
 	message: string
 }
