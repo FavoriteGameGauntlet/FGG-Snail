6647db35feb9988bec63e19f664209445aefd566
Welnyr <thekotech@gmail.com>
2026-01-26 20:02:44 +0500

    chore: Fix timer store types

--- src/stores/timerStore.ts
+++ src/stores/timerStore.ts
@@ -5,32 +5,29 @@ import { api } from '../api-facade/api'
 import { TimerState } from '../api-facade/models'
 import { StoreName } from '../enums/storeName'
 
-const TWO_HOURS_IN_S = 2 * 60 * 60
-
-const getTimeFromSeconds = (seconds: number) =>
-	Temporal.PlainTime.from({
-		hour: Math.floor(seconds / 3600),
-		minute: Math.floor((seconds / 60) % 60),
-		second: seconds % 60,
-	})
+const twoHours = Temporal.Duration.from({
+	hours: 2,
+	minutes: 0,
+	seconds: 0,
+})
 
 export const useTimerStore = defineStore(StoreName.Timer, () => {
-	const timer = ref(getTimeFromSeconds(TWO_HOURS_IN_S))
+	const timer = ref<Temporal.Duration>(Temporal.Duration.from(twoHours))
 	const state = ref<TimerState | null>(TimerState.Created)
-	const totalDuration = ref(getTimeFromSeconds(TWO_HOURS_IN_S))
+	const totalDuration = ref<Temporal.Duration>(twoHours)
 
 	const init = () => {
 		console.log('[TIMER STORE] init')
 		api.timer
 			.getCurrent()
 			.then((v) => {
-				timer.value = getTimeFromSeconds(v.remainingTimeInS)
-				totalDuration.value = getTimeFromSeconds(v.durationInS)
+				timer.value = v.remainingTime
+				totalDuration.value = v.duration
 				state.value = v.state
 			})
 			.catch(() => {
-				timer.value = getTimeFromSeconds(TWO_HOURS_IN_S)
-				totalDuration.value = getTimeFromSeconds(TWO_HOURS_IN_S)
+				timer.value = Temporal.Duration.from(twoHours)
+				totalDuration.value = Temporal.Duration.from(twoHours)
 				state.value = null
 			})
 	}
