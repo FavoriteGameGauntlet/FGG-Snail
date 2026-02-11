56fcd0d29e424026f9fc6ba2325c8d7795956bee
Welnyr <thekotech@gmail.com>
2026-02-11 19:13:14 +0500

    fix: Make main content scrollable

--- src/views/Root/RootView.vue
+++ src/views/Root/RootView.vue
@@ -13,7 +13,9 @@ useEffectStore().init()
 	<div class="grid h-full grid-rows-[auto_1fr]">
 		<AppNavbar class="h-16" />
 
-		<div class="h-full w-full">
+		<div
+			class="h-full max-h-full min-h-0 w-full max-w-full min-w-0 overflow-auto"
+		>
 			<router-view />
 		</div>
 	</div>
